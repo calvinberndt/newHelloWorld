9510402cc741ffc2c2f5f6e073726e16b3ff0021
Calvin Berndt <calvinwberndtjr@gmail.com>
2018-09-17 09:24:19 -0500

    Added Original HelloWorld w/Code

---
+++ HelloWorldProject/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if (lblDisplay.Text == "Hello World")
+            {
+                lblDisplay.Text = "Close Application";
+            }
+
+            else
+            {
+                lblDisplay.Text = "Hello World";
+            }
+        }
+    }
+
+}
